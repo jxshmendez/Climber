1e58bcf1ffb9b527a8ef10be4e8d2b268eaedee6
Joshua Mendez <6joshmendez@gmail.com>
2025-11-10 22:36:12 +0000

    BACKEND-16: Added PostController, Service and Repo

--- backend/src/main/java/org/josh/climber/model/UserModel.java
+++ backend/src/main/java/org/josh/climber/model/UserModel.java
@@ -34,6 +34,7 @@ public class UserModel {
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
 
+
     /* FK */
     @Builder.Default
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
